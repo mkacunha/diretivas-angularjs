8db93e3e15ac2a117d8570e6111b4cb37e3e5541
mkacunha <mkacunha@gmail.com>
2016-08-30 21:42:02 -0300

    exemplo de remover itens de uma lista implementado

--- modal-message/controller.js
+++ modal-message/controller.js
@@ -1,19 +1,26 @@
 var app = angular.module('exemplo-diretivas');
 
 app.controller('ExemploModalMessageController', function($scope){
-  $scope.piadas = [
-    {"id":"1", "piada":"O palmeiras tem mundial... ??"},
-    {"id":"2", "piada":"O corinthians tem dois mundiais... :)"},
-    {"id":"3","piada":"Programador depende apenas de beleza e paciência. Se funcionar, beleza. Senão, paciência."},
-    {"id":"4","piada":"Estágio para programador: saber programar Java,PHP,C#,C,C++,Python,Ruby,Objective-C,Visual basic,Perl,css,html,javascript,AngularJS,Jquery e inglês avançado... :p"}
+  var veiculos = [
+    {"id":"1", "veiculo":"Volkswagen Fusca 1979 vermelho"},
+    {"id":"2", "veiculo":"Chevrolet Onix 2016 branco"},
+    {"id":"3","veiculo":"Fiat Palio 2000 prata"},
+    {"id":"4","veiculo":"Ford Ranger 2015 preta"}
   ]
 
-  $scope.remover = function(piada){
-    var indice = $scope.piadas.indexOf(piada);
-    $scope.piadas.splice(indice, 1);
+  $scope.veiculos = angular.copy(veiculos);
+
+
+  $scope.remover = function(veiculo){
+    var indice = $scope.veiculos.indexOf(veiculo);
+    $scope.veiculos.splice(indice, 1);
   }
 
-  $scope.cancelou = function(piada){
-    console.log('Usuário cancelou a remoção da piada: ' + piada.piada);
+  $scope.cancelou = function(veiculo){
+    console.log('Usuário cancelou a remoção do veículo: ' + veiculo.veiculo);
+  }
+
+  $scope.carregarVeiculos = function(){
+    $scope.veiculos = angular.copy(veiculos);
   }
 });
